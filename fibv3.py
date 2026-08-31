e62237c1194b82f3bef156e0e7825dfd83396c85
d <clxmr3@gmail.com>
2026-08-31 15:54:02 -0400

    finish v3 and changed v1 a bit

---
+++ fibv3.py
@@ -0,0 +1,24 @@
+def fib_fast(n):
+    binary = bin(n)[2:]
+
+    def f_even(a,b):
+        return 2*b*a - a*a
+
+    def f_odd(a,b):
+        return a**2 + b**2
+
+    a = 0
+    b = 1
+
+    for bit in binary:
+        c = f_even(a,b)
+        d = f_odd(a,b)
+
+        if bit == '0':
+            a,b = c,d
+        else:
+            a,b = d, c + d
+    return a
+
+n = int(input("what num of fib would you like: "))
+print(fib_fast(n))
--- initalfib.py
+++ initalfib.py
@@ -1,14 +1,15 @@
+def fib_v1(loop):
+    n = 0
+    n1 = 0
+    n2 = 1
+
+    while n < loop:
+        temp = n1
+        n1 = n2
+        n2 = temp + n2
+        n += 1
+
+    return n1
+
 loop = int(input("what num of fib would you like: "))
-n = 0
-num = 0
-
-n1 = 0
-n2 = 1
-
-while n < loop:
-    temp = n1
-    n1 = n2
-    n2 = temp + n2
-    n += 1
-
-print(n1)
+print(fib_v1(loop))
